ca08bd0052fe79797c672291819bdc85674bcca6
David Knoll <knolladavid@gmail.com>
2015-04-13 22:28:12 -0600

    final commit

--- Disemvoweler/Disemvoweler/Program.cs
+++ Disemvoweler/Disemvoweler/Program.cs
@@ -1,27 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Disemvoweler
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Disemvoweler("Nickelback is my favorite band. Their songwriting can't be beat!");
+            Disemvoweler("How many bears could bear grylls grill if bear grylls could grill bears?");
+            Disemvoweler("I'm a code ninja, baby. I make the functions and I make the calls.");
             // keep the console open
             Console.ReadKey();
         }
-        public static string Disemvoweler(string input)
+
+        public static string Disemvoweler(string guitarString)
         {
+            //variable called "inVowels" - leave blank
+            var inVowels = "";
+            //variable called "inConstants" - leave blank
+            var inConstants = "";
+            //take "guitarString" input & change chars to lower case 
+            var lower = guitarString.ToLower();
+
+            for (var i = 0; i <= guitarString.Length - 1; i++)
+            {
+                // this will check the [i] variable for a vowel or special character
+                if (lower[i].ToString() == "a" || lower[i].ToString() == "e" || lower[i].ToString() == "i" || lower[i].ToString() == "o" || lower[i].ToString() == "u" || lower[i].ToString() == "." || lower[i].ToString() == "," || lower[i].ToString() == "?" || lower[i].ToString() == "'" || lower[i].ToString() == "!" || lower[i].ToString() == " ")
+                {
+                    //if true add the char to "inVowels
+                    inVowels = inVowels + lower[i].ToString();
+                }
+                else
+                {
+                    //else add the char to "inConstants
+                    inConstants = inConstants + lower[i].ToString();
+                }
+            }
 
             // Write out the various string results
-            Console.WriteLine("Original: ");
-            Console.WriteLine("Disemvoweled: ");
-            Console.WriteLine("Vowels Removed: ");
-            // Return the Disemvoweled string as well for testing
-            return string.Empty;
+
+            
+            Console.WriteLine("Original: " + guitarString);
+            Console.WriteLine("Disemvoweled: " + inConstants);
+            Console.WriteLine("Vowels Removed: " + inVowels);
+            return inConstants;
         }
     }
 }
